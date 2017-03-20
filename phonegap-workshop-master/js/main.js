e1dc8c6645d12ea3153f49c9da1a5a60b4ee353c
CoxAdam1701 <cox.ammon@gmail.com>
2017-03-20 15:20:51 -0400

    Updated

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -14,10 +14,17 @@ var app = {
     },
 
     initialize: function() {
+        var self = this;
         this.store = new MemoryStore();
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+    showAlert: function(message, title){
+        if(navigator.notification){
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title +": "+message) : message);
+        }
     }
-
 };
 
 app.initialize();
