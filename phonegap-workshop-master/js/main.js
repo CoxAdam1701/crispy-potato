c88b79bf6f0138d56662940f6ad2162dddfd9a68
CoxAdam1701 <cox.ammon@gmail.com>
2017-03-20 15:26:05 -0400

    Fixed it

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -15,7 +15,9 @@ var app = {
 
     initialize: function() {
         var self = this;
-        this.store = new MemoryStore();
+        this.store = new MemoryStore(function(){
+            self.showAlert('Store Initialized', 'Info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
     showAlert: function(message, title){
