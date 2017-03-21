f1bcb6152acd4bba66c4cb5f9b60f10901733945
CoxAdam1701 <cox.ammon@gmail.com>
2017-03-21 10:32:22 -0400

    updated from master

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -1,12 +1,27 @@
 var app = {
 
-    initialize: function() {
+    registerEvents: function() {
         var self = this;
-        this.registerEvents();
-        this.store = new MemoryStore(function(){
-            $('body').html(new HomeView(self.store).render().el);
-        });
+        // Check of browser supports touch events...
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            // ... if yes: register touch event listener to change the "selected" state of the item
+            $('body').on('touchstart', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            // ... if not: register mouse events instead
+            $('body').on('mousedown', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        }
     },
+
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -14,27 +29,15 @@ var app = {
             alert(title ? (title + ": " + message) : message);
         }
     },
-    registerEvents: function() {
-        var self=this;
-        //Check if browser supports touch events
-        if(document.documentElement.hasOwnProperty('ontouchstart')) {
-        $('body').on('touchstart', 'a', function(event) {
-            $(event.target).addClass('tappable-active');
-        });
-        $('body').on('touchend', 'a', function(event){
-            $(event.target).removeClass('tappable-active');
-        });
-        } else {
-            // if not register mouse events instead
-            $('body').on('mousedown', 'a', function(event){
-                $(event.target).addClass('tappable-active');
-            });
-            $('body').on('mouseup', 'a', function(event){
-                $(event.target).removeClass('tappable-active');
-            });
-        }
 
+    initialize: function() {
+        var self = this;
+        this.registerEvents();
+        this.store = new MemoryStore(function() {
+            $('body').html(new HomeView(self.store).render().el);
+        });
     }
+
 };
 
 app.initialize();
